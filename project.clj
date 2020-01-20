47ea7bf2d25eefb60713335a876141c215451c0e
Ben Grabow <ben.grabow@gmail.com>
2020-01-20 00:25:14 -0600

    Prep library dependencies for flight

--- project.clj
+++ project.clj
@@ -4,4 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.walmartlabs/lacinia "0.35.0"]])
+                 [com.stuartsierra/component "0.4.0"]
+                 [com.walmartlabs/lacinia "0.35.0"]
+                 [com.walmartlabs/lacinia-pedestal "0.12.0"]
+                 [org.clojure/java.jdbc "0.7.10"]
+                 [org.postgresql/postgresql "42.2.8.jre7"]
+                 [com.mchange/c3p0 "0.9.5.4"]
+                 [io.aviso/logging "0.3.2"]])
